4f023d4750d8d75bb1b1c263f03e54e1bb908b23
Muhamad Rifaldi <aldi44375@gmail.com>
2025-05-25 21:44:58 +0700

    routes fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-
+import { Routes, Route } from 'react-router-dom';
 import HeroSection from './components/HeroSection';
-// import './assets/App.css'; Opsional: tambahan styling global
+import About from './routes/About';
+import Contact from './routes/Contact';
+import Support from './routes/Support';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <HeroSection />
-    </>
+    <Routes>
+      <Route path="/" element={<HeroSection />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/support" element={<Support />} />
+    </Routes>
   );
 }
 
